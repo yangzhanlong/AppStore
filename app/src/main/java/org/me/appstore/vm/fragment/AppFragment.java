688a61e5c23453216501a604d6a17ede00a68c6d
Zhanlong Yang <673029686@qq.com>
2018-03-11 10:43:22 +0800

    Add app fragment

--- app/src/main/java/org/me/appstore/vm/fragment/AppFragment.java
+++ app/src/main/java/org/me/appstore/vm/fragment/AppFragment.java
@@ -2,12 +2,16 @@ package org.me.appstore.vm.fragment;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,21 +24,94 @@ import java.util.List;
  * 应用
  */
 public class AppFragment extends Fragment {
-    ListView listView;
+
+    private boolean isReadData = true;
+    private boolean isNullData = false;
+    private FrameLayout app_container;
+    private View loading;
+    private View error;
+    private View empty;
+    private ListView listView;
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (isReadData) {
+                if (isNullData) {
+                    showEmpty();
+                } else {
+                    showSuccess();
+                }
+            } else {
+                showError();
+            }
+        }
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_app, null);
-        return view;
+        app_container = (FrameLayout) inflater.inflate(R.layout.fragment_app, null);
+        return app_container;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        listView = (ListView) view.findViewById(R.id.listview);
+    public void onResume() {
+        super.onResume();
+        dymania();
+    }
+
+    private void dymania() {
+        showProgress();
+        loadData();
+    }
+
+    private void showProgress() {
+        loading = View.inflate(getContext(), R.layout.pager_loading, null);
+        app_container.removeAllViews();
+        app_container.addView(loading);
+    }
+
+    private void showError() {
+        error = View.inflate(getContext(), R.layout.pager_error, null);
+        app_container.removeAllViews();
+        app_container.addView(error);
+
+        error.findViewById(R.id.error_btn_retry).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dymania();
+            }
+        });
+    }
+
+    private void showEmpty() {
+        empty = View.inflate(getContext(), R.layout.pager_empty, null);
+        app_container.removeAllViews();
+        app_container.addView(empty);
+    }
+
+    private void showSuccess() {
+        listView = (ListView) View.inflate(getContext(), R.layout.app_success, null);
 
         testData();
         listView.setAdapter(new AppAdapter());
+
+        app_container.removeAllViews();
+        app_container.addView(listView);
+    }
+
+    private void loadData() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                SystemClock.sleep(2000);
+                handler.sendEmptyMessage(10);
+            }
+        }.start();
     }
 
     private List<String> mDatas;
